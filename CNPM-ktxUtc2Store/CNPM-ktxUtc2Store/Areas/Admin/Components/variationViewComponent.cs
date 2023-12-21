2a0b80906c1a917b4f424d8fe5f4b162a72395c2
fuochung0905 <6251071033@st.utc2.edu.vn>
2023-12-21 16:12:42 +0700

    variation

--- CNPM-ktxUtc2Store/CNPM-ktxUtc2Store/Areas/Admin/Components/variationViewComponent.cs
+++ CNPM-ktxUtc2Store/CNPM-ktxUtc2Store/Areas/Admin/Components/variationViewComponent.cs
@@ -1,37 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CNPM_ktxUtc2Store.Areas.Admin.Components
 {
-    public class categoriesViewComponent : ViewComponent
+    public class variationViewComponent : ViewComponent
     {
         private readonly ApplicationDbContext _context;
-        public categoriesViewComponent(ApplicationDbContext dbContext)
+        public variationViewComponent(ApplicationDbContext dbContext)
         {
             _context = dbContext;
         }
         public IViewComponentResult Invoke()
-
         {
-
+           
             var categories = _context.categories.ToList();
-            
-            var variotions = _context.variation.ToList();
-
-            foreach (var ca in categories)
+            var variotions=_context.variation.ToList();
+            foreach(var ca in categories)
             {
-                ca.variations.Clear();
                 foreach (var item in variotions)
                 {
-
-                    if (ca.Id == item.categoryId)
-                    {
-
+                    if(ca.Id == item.categoryId) {
                         ca.variations.Add(item);
                     }
                 }
-
             }
-
             return View(categories);
         }
     }
